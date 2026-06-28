69cb787e940f5e4a5463c2885ea28c13fc8f639e
notrojo22 <rosanroan@gmail.com>
2026-06-28 14:55:03 -0400

    Configured GitHub Pages settings

--- CS39548-01-3047-Homework-2-/vite.config.js
+++ CS39548-01-3047-Homework-2-/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages configuration
+  // https://notrojo22.github.io/CSCI-39548-01-3047-Homework-2-/
+  base: '/CSCI-39548-01-3047-Homework-2-/',
 })
